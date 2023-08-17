fa925b2505bf75300b0a7e8b3439990cbd41b78d
Fikayo Jetawo <jetawof@gmail.com>
2023-08-17 01:28:08 +0100

    Generating Qr code to be used

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const qr = require("qr-image");
+const fs = require("fs");
+
+const urlToLink = "https://shorturl.at/txNSZ"; // Replace this with your desired URL
+
+// Function to generate and save the QR code image
+const generateQRCode = () => {
+  const qrCode = qr.image(urlToLink, { type: "png" });
+  qrCode.pipe(fs.createWriteStream("qr_code.png"));
+};
+
+// Generate the QR code on server startup
+generateQRCode();
+
+// Route to serve the QR code image
+app.get("/qr", (req, res) => {
+  res.sendFile(`${__dirname}/qr_code.png`);
+});
+
+app.listen(8080, () => {
+  console.log("Server is listening");
+});
