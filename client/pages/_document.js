819c82e853e0f3b0dbea0d32494d95edeeb438e5
Olufikayomi Jetawo <jetawof@gmail.com>
2023-11-09 10:07:36 -0800

    website seo and fav icon

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>TOLoveForever</title>
+        <meta name='description' content="Odunayo Jetawo Weds Tobiloba Oduwole"/>
+        <meta name="keywords" content='odunayo, jetawo, toloveforever, tobiloba, oduwole , wedding, love, forever , to'/>
+      </Head>
       <body>
         <Main />
         <NextScript />
